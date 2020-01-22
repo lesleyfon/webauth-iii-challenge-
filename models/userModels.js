58d96f36ff5838fb19b8ded650202c12ed0e0824
lesley <lesleybanadzem2017@gmail.com>
2020-01-22 12:54:25 -0800

    created an addUser model to add a user to the db and hash password before adding user into the db and created a register route for adding new user

--- models/userModels.js
+++ models/userModels.js
@@ -1,10 +1,22 @@
 const db = require('./../data/dbConfig');
+const bcrypt = require('bcryptjs');
 
 function getUsers (){
     return db('users')
 }
 
+async function addUser(user){
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+    const [id] = await db('users').insert(user);
+    return findUserById(id)
+}
 
+function findUserById(id){
+    return db('users').where('id', id).select('id', 'username').first();
+}
 module.exports = {
-    getUsers
+    getUsers,
+    addUser,
+    findUserById
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('./../models/userModels.js')
+const User = require("./../models/userModels.js");
 
+router.get("/users", async (req, res, next) => {
+  res.json({
+    users: await User.getUsers()
+  });
+});
 
-
-router.get('/users' , async (req, res, next) =>{
-    res.json({
-       users: await User.getUsers() 
-    })
-})
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      res.status(404).json({
+        error: "Please make sure you provide a username and password",
+        username: username,
+        password: password
+      });
+    } else {
+      res.status(200).json({
+        message: "Successfully added a new user",
+        user: await User.addUser({ username, password })
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      error: error
+    });
+  }
+});
 
 module.exports = router;

4920abba357939fb11204358543997d099ca72c2
lesley <lesleybanadzem2017@gmail.com>
2020-01-23 12:04:09 -0800

    Added a logout route to log user out and destroyn session

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -65,4 +65,11 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+router.get('/logout', (req, res, next)=>{
+  req.session.destroy();
+  res.status(200).json({
+    message: 'You have been logged out successfully'
+  })
+})
+
 module.exports = router;
